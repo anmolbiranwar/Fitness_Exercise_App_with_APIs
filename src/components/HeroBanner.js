7afb920ad93a5c8dd119297f13b4dda9e00cefc2
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-02 13:00:58 -0700

    Update HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -7,11 +7,11 @@ const HeroBanner = () => {
   return (
     <Box sx={{ mt: { lg: '100px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="25px">Fitness Club</Typography>
-     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="3px" mt="3px">
+     <Typography fontWeight={700} sx={{ fontSize: { lg: '40px', xs: '35px' } }} mb="2px" mt="2px">
        Sweat, Smile <br />
        And Repeat
      </Typography>
-     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="20px">
+     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="25px">
        Check out the most effective exercises personalized to you
      </Typography>
      <Stack>
