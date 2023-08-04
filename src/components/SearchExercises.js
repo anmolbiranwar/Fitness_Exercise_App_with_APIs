a217a068c289808caf1690dad66f61e222ff876b
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-03 20:10:47 -0700

    Update SearchExercises.js

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,10 +4,29 @@ import {Box, Button, Stack, TextField, Typography} from '@mui/material'
 const SearchExercises = () => {
   return (
     <Stack alignItems="center" mt="37px"  justifyContent="center" p="20px">
-      <Typography>
+      <Typography fontWeight={700} sx={{fontSize:{lg:"44px", xs:"30px"}}} mb="50px" textAlign="center">
         Awesome Exercises You <br/>
         Should Know
       </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+        sx={{
+          input:{
+            fontWeight:"700px",
+            border:"none",
+            borderRadius:"4px"
+          },
+          width:{lg:"1170px",xs:"350px"},
+          backgroundColor:"#fff",
+          borderRadius:"40px"
+        }}
+        height="760"
+        value=""
+        onChange={(e)=>{}}
+        placeholder='Search Exercises'
+        type="text"
+         />
+      </Box>
     </Stack>
   )
 }
